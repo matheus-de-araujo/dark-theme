9b2313b17e565e2f1319ba577473ac3cb30cc934
Matheus de Araujo <matheus@pop-os.localdomain>
2021-09-15 10:01:59 -0300

    commit initial

---
+++ load.js
@@ -0,0 +1,35 @@
+function darkenSetState(state) {
+  if (state === "1") {
+    document.documentElement.classList.toggle("nodarken", false);
+    document.documentElement.classList.toggle("darkeni", false);
+    chrome.runtime.sendMessage({state: "1"});
+  } else if (state === "2") {
+    document.documentElement.classList.toggle("nodarken", false);
+    document.documentElement.classList.toggle("darkeni", true);
+    chrome.runtime.sendMessage({state: "2"});
+  } else if (state === "3") {
+    document.documentElement.classList.toggle("nodarken", true);
+    document.documentElement.classList.toggle("darkeni", true);
+    chrome.runtime.sendMessage({state: "3"});
+  } else if (state === "4") {
+    document.documentElement.classList.toggle("nodarken", true);
+    document.documentElement.classList.toggle("darkeni", false);
+    chrome.runtime.sendMessage({state: state});
+  }
+}
+
+function darkenFromState() {
+  var state = localStorage.getItem("darken-state");
+  if (state === null) {
+    state = localStorage.getItem("darken-default-state");
+    chrome.runtime.sendMessage({setting: "need-default-state"}, function(response) {
+      if (state != response) {
+        state = response;
+        localStorage.setItem("darken-default-state", state);
+        darkenSetState(state)
+      }
+    });
+  }
+  darkenSetState(state);
+};
+darkenFromState();
