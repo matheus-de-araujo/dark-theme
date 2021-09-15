912976209b1ec347df7911b27f689ffbe24ef84b
Matheus de Araujo <matheus@pop-os.localdomain>
2021-09-15 15:51:45 -0300

    Ajustes no css

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function darkenNextState() {
   var state = localStorage.getItem("darken-state");
   if (state === null)
-    state = localStorage.getItem("darken-default-state");
+    state = localStorage.getItem("darken-state", "1");
   if (state === "1" || state === null) {
     localStorage.setItem("darken-state", "2");
   } else {
